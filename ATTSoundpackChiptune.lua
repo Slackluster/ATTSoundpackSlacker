49cc086352ba42d3c652d8e4ebe9603c523fbb52
Sluimerstand <Ruben.Sluimer@gmail.com>
2023-05-23 19:56:48 +0200

    File paths fixed as a result of the CurseForge-imposed rename

--- ATTSoundpackChiptune.lua
+++ ATTSoundpackChiptune.lua
@@ -15,20 +15,20 @@ end
 
 -- Add custom ATT sounds
 local function addSounds()
-    AllTheThings.AddSound("Complete", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\complete1.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare1.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare2.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare3.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare4.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare5.ogg")
-    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\fanfare6.ogg")
-    AllTheThings.AddSound("RareFind", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\rarefind1.ogg")
-    AllTheThings.AddSound("Remove", "Interface\\AddOns\\AllTheThings_Soundpack_Pokemon\\assets\\remove1.ogg")
+    AllTheThings.AddSound("Complete", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\complete1.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare1.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare2.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare3.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare4.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare5.ogg")
+    AllTheThings.AddSound("Fanfare", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\fanfare6.ogg")
+    AllTheThings.AddSound("RareFind", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\rarefind1.ogg")
+    AllTheThings.AddSound("Remove", "Interface\\AddOns\\ATTSoundpackChiptune\\assets\\remove1.ogg")
 end
 
 api:SetScript("OnEvent", function(self, event, arg1, arg2, ...)
 	-- When the AddOn is fully loaded, actually run the components
-	if event == "ADDON_LOADED" and arg1 == "AllTheThings_Soundpack_Pokemon" then
+	if event == "ADDON_LOADED" and arg1 == "ATTSoundpackChiptune" then
         -- We don't need to check if ATT is also loaded, because it is a dependency for this AddOn
         removeSounds()
         addSounds()

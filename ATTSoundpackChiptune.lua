aa1a4155f6e7f37daaa6015b4e10cd12e2ab5605
Sluimerstand <Ruben.Sluimer@gmail.com>
2023-10-11 22:12:21 +0200

    Spaces to tabs (thanks Crieve >,>)

--- ATTSoundpackChiptune.lua
+++ ATTSoundpackChiptune.lua
@@ -8,27 +8,27 @@ local api = AllTheThings.Audio
 api:CreateSoundPack("Pokémon (" .. appName .. ")", {
 	COMPLETE = {
 		asset("evo_gen2.ogg"),
-        asset("evo_gen3.ogg"),
-        asset("evo_gen4.ogg"),
-        asset("evo_gen5.ogg"),
-        asset("evo_gen6.ogg"),
+		asset("evo_gen3.ogg"),
+		asset("evo_gen4.ogg"),
+		asset("evo_gen5.ogg"),
+		asset("evo_gen6.ogg"),
 	},
 	DEATH = {
 		asset("death.ogg"),
 	},
 	FANFARE = {
 		asset("badge_gen2.ogg"),
-        asset("badge_gen3.ogg"),
-        asset("badge_gen4.ogg"),
-        asset("badge_gen5.ogg"),
-        asset("badge_gen6.ogg"),
+		asset("badge_gen3.ogg"),
+		asset("badge_gen4.ogg"),
+		asset("badge_gen5.ogg"),
+		asset("badge_gen6.ogg"),
 	},
 	RAREFIND = {
 		asset("tm_gen2.ogg"),
-        asset("tm_gen3.ogg"),
-        asset("tm_gen4.ogg"),
-        asset("tm_gen5.ogg"),
-        asset("tm_gen6.ogg"),
+		asset("tm_gen3.ogg"),
+		asset("tm_gen4.ogg"),
+		asset("tm_gen5.ogg"),
+		asset("tm_gen6.ogg"),
 	},
 	REMOVE = {
 		asset("gamecorner-lose.ogg"),
@@ -41,7 +41,7 @@ api:CreateSoundPack("Pokémon (" .. appName .. ")", {
 api:CreateSoundPack("Kazoo (" .. appName .. ")", {
 	COMPLETE = {
 		asset("kazooc1.ogg"),
-        asset("kazooc2.ogg"),
+		asset("kazooc2.ogg"),
 	},
 	DEATH = {
 		asset("kazoo.ogg"),
